827496067a3226fffa80a106ea4593c15993e600
aiechevarria <184414920+aiechevarria@users.noreply.github.com>
2025-12-06 12:02:49 +0100

    Added sprites for all items and tiles

--- src/graphics.h
+++ src/graphics.h
@@ -1,16 +1,58 @@
 /**
- * All graphic sprites
+ * All graphics, sprites and colors.
  * 
  * @authors Aitor Echevarría Floranes, Rubén San Pedro.
  */
 
-#include "items.h"
 #include "levels.h"
 
-#define SPRITE_SIZE 3
+#define SPRITE_SIZE 12
+
+// Color mappings
+#define RS_RGB {0xff, 0x00, 0x00}
+#define RL_RGB {0xff, 0x43, 0x43}
+#define GS_RGB {0x11, 0xB9, 0x48}
+#define GL_RGB {0x31, 0xD7, 0x68}
+#define BS_RGB {0x00, 0x00, 0xff}
+#define BL_RGB {0x20, 0x71, 0xff}
+#define PS_RGB {0x73, 0x00, 0x98}
+#define PL_RGB {0x82, 0x09, 0xc5}
+#define OS_RGB {0xf4, 0x63, 0x05}
+#define OL_RGB {0xf4, 0x84, 0x3c}
+#define SP_RGB {0xf4, 0xf4, 0x00}
+#define WH_RGB {0xfe, 0xfe, 0xfe}
+#define BK_RGB {0x00, 0x00, 0x00}
+#define C1_RGB {0xc0, 0xc0, 0xc0}
+#define C2_RGB {0x80, 0x80, 0x80}
+#define TS_RGB {0xff, 0xff, 0xff}
+
+// Color names
+typedef enum {
+    RS,         // Red strong
+    RL,         // Red light
+    GS,         // Green strong
+    GL,         // Green light
+    BS,         // Blue strong
+    BL,         // Blue light
+    PS,         // Purple strong
+    PL,         // Purple light
+    OS,         // Orange strong
+    OL,         // Orange light
+    SP,         // Special
+    WH,         // White
+    BK,         // Black
+    C1,         // Corruption 1
+    C2,         // Corruption 2
+    TS,         // Transparent
+    NUM_COLORS
+} Colors;
+
+typedef struct {
+	unsigned char r, g, b;
+} Pixel;
 
 // Item Sprites
-const char itemSprites[NUM_ITEM_GRADES][SPRITE_SIZE][SPRITE_SIZE] = {
+const char itemSprites[NUM_ITEMS][SPRITE_SIZE][SPRITE_SIZE] = {
     { 
         {'0', '0', '0'},                // Normal item
         {'0', '1', '0'},
@@ -38,6 +80,6 @@ const char itemSprites[NUM_ITEM_GRADES][SPRITE_SIZE][SPRITE_SIZE] = {
     }
 };
 
-const char tileSprites[NUM_TILES][TILE_SIZE][TILE_SIZE] = {
+const char tileSprites[NUM_TILES][SPRITE_SIZE][SPRITE_SIZE] = {
     // TODO THis should be drawn in the background
 };
--- src/levels.h
+++ src/levels.h
@@ -5,29 +5,40 @@
  */
 
 #define MAX_LEVELS      3
-#define MAX_X_COORDS    10
-#define MAX_Y_COORDS    10
 
-#define TILE_SIZE 5
+// Number of elements in a level
+#define MAX_X_SPRITES    10
+#define MAX_Y_SPRITES    10
 
-/**
- * The different types of tiles a level can have.
- */
+// The different types of tiles a level can have. Drawn on the background
 typedef enum {
     EMPTY,              // An item can be placed on top
-    JELLY,              // An item can be placed on top and turns to EMPTY once broken
-    DENSE,              // Denser JELLY, breaking it results in JELLY
+    CORPT,              // An item can be placed on top and turns to EMPTY once broken
+    DENSE,              // Denser CORPT, breaking it results in CORPT
     UNFIL,              // Unfillable space, for levels that have more complex layout
     NUM_TILES,
 } Tile;
 
-/**
- * The different types of objectives a level can have.
- */
+// All possible types of items. Drawn on the foreground, unless the tile is of type UNFILL
 typedef enum {
-    OBJ_DESTROY_JELLY,      // Destroy all the jelly in a level
-    OBJ_DESTROY_COLOR,      // Destroy a certain number of elements of a color
-    OBJ_GET_SCORE           // Get a lot of score in this level;
+    ITEM_RED,
+    ITEM_ORANGE,
+    ITEM_GREEN,
+    ITEM_BLUE,
+    ITEM_PURPLE,
+    ITEM_ROW,           // Clears an entire row
+    ITEM_COLUMN,        // Clears an entire column
+    ITEM_SPARK,         // Clears the adjacent 9 tiles
+    ITEM_BOMB,          // Clears all the items in the level that are of the same type
+    NUM_ITEMS
+} Item;
+
+// The different types of objectives a level can have.
+typedef enum {
+    OBJ_DESTROY_CORPT,      // Destroy all the corruption in a level
+    OBJ_DESTROY_ITEM,       // Destroy a certain number of items
+    OBJ_GET_SCORE,          // Get a lot of score in this level;
+    NUM_OBJECTIVES
 } Objective;
 
 typedef struct {
@@ -37,20 +48,21 @@ typedef struct {
     Objective objective;
 
     // Params for particular objectives
-    // OBJ_DESTROY_JELLY
-    uint8 objJellyRemaining;    // How many jellies are left. Has to be prefilled
+    // OBJ_DESTROY_CORPT
+    uint8 objCorptRemaining;    // How many corruption tiles are left. Has to be prefilled
 
-    // OBJ_DESTROY_COLOR
-    ItemColor objColorType;     // What color to destroy
-    uint16 objColorDestroy;     // How many of that color to destroy
-    uint16 objScore;            // How many points to 
+    // OBJ_DESTROY_ITEM
+    Item objItemType;          // What item to destroy
+    uint16 objItemDestroy;     // How many of that color to destroy
+
+    // OBJ_GET_SCORE
+    uint16 objScore;           // How much score should be reached to end the level
 
     // Parameters that are updated on each move. This gets copied on init
     uint8 moves;            // Remaining moves
     uint8 score;            // Current score
-    Tile level[MAX_X_COORDS][MAX_Y_COORDS];     // Current state of the level
+    Tile level[MAX_X_SPRITES][MAX_Y_SPRITES];     // Current state of the level
 } LevelInfo;
-
 
 const LevelInfo levels[MAX_LEVELS] = {
     {
@@ -59,16 +71,16 @@ const LevelInfo levels[MAX_LEVELS] = {
         .objective = OBJ_DESTROY_JELLY,
         .objJellyRemaining = 10,
         .level = {
-            {JELLY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-            {EMPTY, JELLY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-            {EMPTY, EMPTY, JELLY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-            {EMPTY, EMPTY, EMPTY, JELLY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-            {EMPTY, EMPTY, EMPTY, EMPTY, JELLY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, JELLY, EMPTY, EMPTY, EMPTY, EMPTY},
-            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, JELLY, EMPTY, EMPTY, EMPTY},
-            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, JELLY, EMPTY, EMPTY},
-            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, JELLY, EMPTY},
-            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, JELLY}
+            {CORPT, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, CORPT, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, CORPT, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, CORPT, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, CORPT, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, CORPT, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, CORPT, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, CORPT, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, CORPT, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, CORPT}
         }
     },
     {
