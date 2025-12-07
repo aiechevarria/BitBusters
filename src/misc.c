0e11ee5aeca51051e32c52d55484f42c61d0e7b6
aiechevarria <184414920+aiechevarria@users.noreply.github.com>
2025-12-07 15:07:30 +0100

    Added miscelaneous functions file

---
+++ src/misc.c
@@ -0,0 +1,15 @@
+#include "misc.h"
+
+unsigned int rngState = SEED;
+
+/**
+ * Returns a pseudorandom number.
+ * 
+ * @return The pseudorandom number in range [0..255] 
+ */
+char pseudoRNG(void) {
+    rngState = (1664525 * rngState + 1013904223);
+
+    // Return the high 8 bits (better than low bits)
+    return (rngState >> 24) & 0xFF;
+}
---
+++ src/misc.h
@@ -0,0 +1,16 @@
+/**
+ * Miscelaneous functions.
+ * 
+ * @authors Aitor Echevarría Floranes, Rubén San Pedro.
+ */
+
+#define SEED 1234
+
+#define true 1
+#define false 0
+
+typedef unsigned char   uint8;
+typedef unsigned short  uint16;
+typedef unsigned int    uint32;
+
+char pseudoRNG(void);
